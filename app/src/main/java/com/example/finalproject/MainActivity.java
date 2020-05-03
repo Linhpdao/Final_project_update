ad4e0440f378f0892b23cae2fe3cc907736b76b8
LENOVO YOGA 260 <linhpd2@illinois.edu>
2020-05-03 22:27:07 +0700

    Toe App 1

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,31 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
-import android.os.SystemClock;
-import android.util.TimeUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     private Button resetButton;
     private Button polishButton;
-    //private Button noPolishButton;
+
+    private TextView cmThing;
+    int count;
 
     private TextView countDown;
     private CountDownTimer countDownTimer;
     private long timeLeftMilliSec = 604800000;
     int week = 5;
     int sec;
+    int currentImage = 1;
 
     private static ImageView imageView;
+    private static ImageView imageViewNoPolish;
+    private static ImageView imageViewYesPolish;
     private static Button noPolishButton;
-    private int currentImage;
-    int[] images = {R.drawable.startlength, R.drawable.troll, R.drawable.startlength};
+    private Handler mHandler = new Handler();
+    int[] images = {R.drawable.troll, R.drawable.startlength, R.drawable.secondlength, R.drawable.thirdlength};
+    int[] noPolish = {R.drawable.no_polish_not_chosen, R.drawable.no_polish_chosen, R.drawable.the_real_not_available};
+    int[] yesPolish = {R.drawable.nail_polish_not_chosen, R.drawable.nail_polish_chosen};
 
-    int[] progressImages = {R.drawable.progrees2, R.drawable.progress3, R.drawable.progress_cuoi};
 
 
 
@@ -39,6 +45,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView =  findViewById(R.id.imageView);
+        imageViewNoPolish = findViewById(R.id.imageViewNoPolish);
+        imageViewYesPolish = findViewById(R.id.imageViewYesPolish);
+
+
+        cmThing = findViewById(R.id.cmThing);
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while (!isInterrupted()) {
+                    try {
+                        Thread.sleep(1000);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                count++;
+                                double cm = count * 0.00000077;
+                                DecimalFormat df = new DecimalFormat("0.00000000");
+                                String cmthingText;
+                                cmthingText = "Toenail growth length: " + df.format(cm) + " cm";
+                                cmThing.setText(cmthingText);
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
 
 
         countDown = findViewById(R.id.countDown);
@@ -50,14 +90,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openOpeningScreen();
+                //mHandler.postDelayed(cRunnable, 1000);
+                //imageView.setImageResource(images[1]);
             }
         }));
 
-        buttonClick();
+
     }
-
-
-
 
 
 
@@ -65,26 +104,54 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void buttonClick () {
-        imageView =  findViewById(R.id.imageView);
+
         noPolishButton = findViewById(R.id.buttonNoNailPolish);
         noPolishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                imageView.setImageResource(images[1]);
-
-                try {
-                    Thread.sleep(2000);
-                    imageView.setImageResource(images[0]);
-
-                } catch (InterruptedException e) {
-                    //System.err.print(e.getMessage());
-                }
+                imageView.setImageResource(images[0]);
+                imageViewNoPolish.setImageResource(noPolish[1]);
+                imageViewYesPolish.setImageResource(yesPolish[0]);
+                mHandler.postDelayed(aRunnable, 2000);
 
             }
         });
 
     }
+
+    public void buttonNotClick() {
+        noPolishButton = findViewById(R.id.buttonNoNailPolish);
+        noPolishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mHandler.postDelayed(bRunnable,0);
+            }
+        });
+
+
+
+    }
+
+
+    public Runnable bRunnable = new Runnable() {
+        @Override
+        public void run() {
+            imageViewNoPolish.setImageResource(noPolish[2]);
+            Toast.makeText(MainActivity.this, "Option no longer available", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+    public Runnable aRunnable = new Runnable() {
+        @Override
+        public void run() {
+            imageView.setImageResource(images[1]);
+            imageViewNoPolish.setImageResource(noPolish[0]);
+            imageViewYesPolish.setImageResource(yesPolish[1]);
+            Toast.makeText(MainActivity.this, "Just kidding you get nail polish anyway", Toast.LENGTH_SHORT).show();
+        }
+    };
 
 
     public void openOpeningScreen() {
@@ -92,11 +159,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openTrollActivity() {
-        Intent intent = new Intent(this, TrollActivity.class);
-        startActivity(intent);
-
-    }
 
     public void startCounting() {
         countDownTimer = new CountDownTimer(timeLeftMilliSec, 1000) {
@@ -116,18 +178,41 @@ public class MainActivity extends AppCompatActivity {
     public void updateTimer() {
 
         int day = (int) timeLeftMilliSec % 604800000 / 86400000;
-        if (day == 0) {
-            week = week - 1;
-        }
         int hour = (int) timeLeftMilliSec % 604800000 % 86400000 / 3600000;
         int min = (int) timeLeftMilliSec % 604800000 % 86400000 % 3600000 / 60000;
         sec = (int) timeLeftMilliSec %604800000 % 86400000 % 3600000 % 60000 / 1000;
         String timeLeftText;
 
-        //timeLeftText = week + " weeks " + days + " day " + hour + " hour " + min + " min " + sec + " sec ";
-        timeLeftText = week + " week " + day + " day " + hour + " hour " + min + " min " + sec + " sec ";
+//        if (day == 0) {
+//            week = week - 1;
+//        }
+        if (sec == 55) {
+            week = week - 1;
+            if (week < 5 && week % 2 != 0) {
+                currentImage++;
+                imageView.setImageResource(images[currentImage]);
+            }
+        }
+
+
+        timeLeftText = week + " weeks " + day + " days " + hour + " hours " + min + " mins " + sec + " secs ";
+
 
         countDown.setText(timeLeftText);
+
+        if (week > 3) {
+            buttonClick();
+        } else {
+            buttonNotClick();
+        }
+
+        if (week == 0 && sec == 0) {
+            alarm();
+        }
+    }
+
+    public void alarm() {
+
     }
 
 }
